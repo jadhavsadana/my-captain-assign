419836788542510d6b6e251045ad99273f04780b
jadhavsadana <111043400+jadhavsadana@users.noreply.github.com>
2022-08-16 23:22:44 +0530

    Create Arru1.py

---
+++ Arru1.py
@@ -0,0 +1,8 @@
+#online editor
+filename =input("input the filename is:")
+f_exts=filename.split(".")
+print("the extension of the file is."+repr(f_exts{-1}))
+
+
+Output
+Extension of the file is:java
