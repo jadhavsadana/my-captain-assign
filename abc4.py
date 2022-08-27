0038dcdc184b7cfbce381975b0ba7917ad4c755d
jadhavsadana <111043400+jadhavsadana@users.noreply.github.com>
2022-08-27 21:40:21 +0530

    Create abc4.py

---
+++ abc4.py
@@ -0,0 +1,17 @@
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+print(char_frequency('sadhna'))
+
+
+
+
+Output:
+{'s'-1, 'a'-2, 'd'-1, 'h'-1, 'n'-1}
+
